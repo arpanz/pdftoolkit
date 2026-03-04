5bb2bf03c16dff2e4f0852f83322c228960d457c
arpanz <arpansinghbsb@gmail.com>
2026-03-04 15:21:45 +0530

    *

--- lib/core/providers/app_provider.dart
+++ lib/core/providers/app_provider.dart
@@ -15,7 +15,7 @@ class AppProvider extends ChangeNotifier {
   bool _isPro = false;
   bool _isDarkMode = true;
   bool _hasSeenOnboarding = false;
-  ColorTheme _colorTheme = ColorTheme.classic;
+  ColorTheme _colorTheme = ColorTheme.ruby;
   bool _initialized = false;
   List<PdfFileModel> _files = [];
   bool _isLoading = false;
@@ -36,7 +36,7 @@ class AppProvider extends ChangeNotifier {
     final themeName = prefs.getString(_colorThemeKey);
     _colorTheme = ColorTheme.values.firstWhere(
       (theme) => theme.name == themeName,
-      orElse: () => ColorTheme.classic,
+      orElse: () => ColorTheme.ruby,
     );
     await _loadFiles(prefs);
     _initialized = true;
